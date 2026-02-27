1696831482bf62f86819417f9d6eba9222d082c9
vryxiaz <carlosfrankleme@gmail.com>
2026-02-27 23:29:37 +0000

    Concluindo o nível avançado

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,63 @@
 #include <stdio.h>
 
+//Movimento da torre
+void moverTorre(int casas){
+    if(casas > 0){
+        printf("Direita\n");
+        moverTorre(casas - 1);
+    }
+}
+
+//Movimento do Bispo
+void moverBispo(int casas){
+    if(casas > 0){
+        for(int i = 0; i < 1; i++){
+        printf("Cima\n");
+        for(int j = 0; j < 1; j++){
+            printf("Direita\n");
+        }
+    }
+    moverBispo(casas - 1);
+    }
+}
+
+//Movimento da Rainha
+void moverRainha(int casas){
+    if(casas > 0){
+        printf("Esquerda\n");
+        moverRainha(casas - 1);
+    }
+}
+
+//Movimento do Cavalo
+void moverCavalo(){
+    for(int i = 0; i < 1; i++){
+        for(int j = 0; j < 2; j++){
+            printf("Cima\n");
+        }
+        printf("Direita\n");
+    }
+}
+
 int main() {
 
 	int i, j;
 	
-	//Movimento da Torre
+	//Chamar movimeto da torre
 	printf("=== MOVIMENTO DA TORRE ===\n");
+	moverTorre(5);
 	
-	for(i = 0; i < 5; i++){
-	    printf("Direita\n");
-	}
-	i = 0;
-	
-	//Movimento do Bispo
+	//Chamar movimento do Bispo
 	printf("\n=== MOVIMENTO DO BISPO ===\n");
+	moverBispo(5);
 	
-	while(i < 5){
-	    printf("Direita\n");
-	    printf("Cima\n");
-	    i++;
-	}
-	i = 0;
-	
-	//Movimento da Rainha
+	//Chamar movimento da Rainha
 	printf("\n=== MOVIMENTO DA RAINHA ===\n");
+	moverRainha(8);
 	
-	do{
-	    printf("Esquerda\n");
-	    i++;
-	}while(i < 8);
-	i = 0;
-	
-	//Movimento do Cavalo
+	//Chamar movimento do Cavalo
 	printf("\n=== MOVIMENTO DO CAVALO ===\n");
-	
-	for(i = 0; i < 1; i++)
-	{
-	    for(j = 0; j < 2; j++)
-	    {
-	        printf("Baixo\n");
-	    }
-	    printf("Esquerda\n");
-	}
+	moverCavalo();
 
 	return 0;
 }
